98cd74e01ad9c4e412305c431a184bb99c79bd44
benlittlenz <benlittle53@gmail.com>
2021-04-03 11:34:52 +1300

    created note class

---
+++ lib/models/Note.dart
@@ -0,0 +1,13 @@
+class Note {
+  String id;
+  String title;
+  DateTime createDateTime;
+  DateTime lastEdited;
+
+  Note({
+    this.id,
+    this.title,
+    this.createDateTime,
+    this.lastEdited
+  });
+}
--- lib/views/note_list.dart
+++ lib/views/note_list.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class NoteList extends StatelessWidget {
+
+  final notes = [
+
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
