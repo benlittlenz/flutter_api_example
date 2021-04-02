3c7d6774e5c6447309da051d78ba56b4c60c2068
benlittlenz <benlittle53@gmail.com>
2021-04-03 11:29:35 +1300

    added note list page

---
+++ lib/views/note_list.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List of notes"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //
+        },
+        child: Icon(Icons.add),
+      ),
+      body: ListView.separated(
+        separatorBuilder: (_, __) => Divider(height: 1, color: Colors.green),
+        itemBuilder: (_, index) {
+          return ListTile(
+            title: Text('Hello', style: TextStyle(color: Theme.of(context).primaryColor)
+            ),
+            subtitle: Text("Last editied on 21/04/21"),
+          );
+
+        },
+        itemCount: 30
+      ),
+    );
+  }
+}
