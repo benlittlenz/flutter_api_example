5ebbecb2740cbc06f1eb1682af77ae04cb70a799
benlittlenz <benlittle53@gmail.com>
2021-04-03 11:38:26 +1300

    creating and display dummy data

--- lib/views/note_list.dart
+++ lib/views/note_list.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_api_requests/models/Note.dart';
 
 class NoteList extends StatelessWidget {
-
   final notes = [
-
+    new Note(
+      id: "1",
+      title: 'Note 1',
+      createDateTime: DateTime.now(),
+      lastEdited: DateTime.now(),
+    ),
+    new Note(
+      id: "2",
+      title: 'Note 2',
+      createDateTime: DateTime.now(),
+      lastEdited: DateTime.now(),
+    ),
+    new Note(
+      id: "3",
+      title: 'Note 3',
+      createDateTime: DateTime.now(),
+      lastEdited: DateTime.now(),
+    ),
   ];
   @override
   Widget build(BuildContext context) {
@@ -18,17 +35,15 @@ class NoteList extends StatelessWidget {
         child: Icon(Icons.add),
       ),
       body: ListView.separated(
-        separatorBuilder: (_, __) => Divider(height: 1, color: Colors.green),
-        itemBuilder: (_, index) {
-          return ListTile(
-            title: Text('Hello', style: TextStyle(color: Theme.of(context).primaryColor)
-            ),
-            subtitle: Text("Last editied on 21/04/21"),
-          );
-
-        },
-        itemCount: 30
-      ),
+          separatorBuilder: (_, __) => Divider(height: 1, color: Colors.green),
+          itemBuilder: (_, index) {
+            return ListTile(
+              title: Text(notes[index].title.toString(),
+                  style: TextStyle(color: Theme.of(context).primaryColor)),
+              subtitle: Text("Last editied on ${notes[index].lastEdited}"),
+            );
+          },
+          itemCount: notes.length),
     );
   }
 }
