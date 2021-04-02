aed34869277f454693a57d88975cd2c35ea54169
benlittlenz <benlittle53@gmail.com>
2021-04-03 11:53:42 +1300

    added note fields

--- lib/views/note_create.dart
+++ lib/views/note_create.dart
@@ -4,7 +4,22 @@ class NoteModify extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Create note'),),
+      appBar: AppBar(
+        title: Text('Create note'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(hintText: 'Note Title'),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'Note Content'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
