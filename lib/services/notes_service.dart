2d8300fd8f854923bb288c4db2b87b8cdeb0c8ba
benlittlenz <benlittle53@gmail.com>
2021-04-03 17:30:09 +1300

    setup api response model

--- lib/services/notes_service.dart
+++ lib/services/notes_service.dart
@@ -1,26 +1,11 @@
+import 'package:flutter_api_requests/models/API_Response.dart';
 import 'package:flutter_api_requests/models/Note.dart';
+import 'package:http/http.dart' as http;
 
 class NotesService {
-  List<Note> getNoteList() {
-    return [
-      new Note(
-        id: "1",
-        title: 'Note 1',
-        createDateTime: DateTime.now(),
-        lastEdited: DateTime.now(),
-      ),
-      new Note(
-        id: "2",
-        title: 'Note 2',
-        createDateTime: DateTime.now(),
-        lastEdited: DateTime.now(),
-      ),
-      new Note(
-        id: "3",
-        title: 'Note 3',
-        createDateTime: DateTime.now(),
-        lastEdited: DateTime.now(),
-      ),
-    ];
+  static const API = 'https://tq-notes-api-jkrgrdggbq-el.a.run.app/';
+
+  Future<APIResponse<List<Note>>> getNoteList() {
+    return http.get(API);
   }
 }
